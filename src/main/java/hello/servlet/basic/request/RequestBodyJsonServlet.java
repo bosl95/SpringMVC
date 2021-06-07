a91374a8066558b7f1e72ff4dede74138f7a75ab
pomo <bosl95@naver.com>
2021-06-07 20:43:10 +0900

    feat: servlet 패키지 추가

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.HelloData;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData = " + helloData.getUsername());
+        System.out.println("helloData = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/new-form.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sori
+  Date: 2021/06/07
+  Time: 2:18 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="save" method="post">
+    username:   <input type="text" name="username"/>
+    age:        <input type="text" name="age" />
+    <button type="submit">전송</button>
+</form>
+</body>
+</html>
